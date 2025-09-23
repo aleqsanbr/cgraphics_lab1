1ea109c538fd4405d563b060340e6048ae48842a
Mikhail Pokhodeev <pokhodeev@sfedu.ru>
2025-09-23 20:37:05 +0300

    Updated solution of HSV convertion.

--- RGB_HSV_Lab/HSV_form.Designer.cs
+++ RGB_HSV_Lab/HSV_form.Designer.cs
@@ -45,83 +45,92 @@
             // pictureBox
             // 
             this.pictureBox.Image = global::RGB_HSV_Lab.Properties.Resources.fruit_test_image;
-            this.pictureBox.Location = new System.Drawing.Point(2, 12);
+            this.pictureBox.Location = new System.Drawing.Point(3, 15);
+            this.pictureBox.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox.Name = "pictureBox";
-            this.pictureBox.Size = new System.Drawing.Size(800, 350);
+            this.pictureBox.Size = new System.Drawing.Size(1067, 431);
             this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox.TabIndex = 2;
             this.pictureBox.TabStop = false;
             // 
             // h_trackBar
             // 
-            this.h_trackBar.Location = new System.Drawing.Point(12, 393);
+            this.h_trackBar.Location = new System.Drawing.Point(16, 484);
+            this.h_trackBar.Margin = new System.Windows.Forms.Padding(4);
             this.h_trackBar.Maximum = 360;
             this.h_trackBar.Name = "h_trackBar";
-            this.h_trackBar.Size = new System.Drawing.Size(206, 45);
+            this.h_trackBar.Size = new System.Drawing.Size(275, 56);
             this.h_trackBar.TabIndex = 3;
             this.h_trackBar.ValueChanged += new System.EventHandler(this.trackBar_ValueChanged);
             // 
             // s_trackBar
             // 
-            this.s_trackBar.Location = new System.Drawing.Point(224, 393);
+            this.s_trackBar.Location = new System.Drawing.Point(299, 484);
+            this.s_trackBar.Margin = new System.Windows.Forms.Padding(4);
             this.s_trackBar.Maximum = 100;
             this.s_trackBar.Minimum = -100;
             this.s_trackBar.Name = "s_trackBar";
-            this.s_trackBar.Size = new System.Drawing.Size(206, 45);
+            this.s_trackBar.Size = new System.Drawing.Size(275, 56);
             this.s_trackBar.TabIndex = 4;
             this.s_trackBar.ValueChanged += new System.EventHandler(this.trackBar_ValueChanged);
             // 
             // v_trackBar
             // 
-            this.v_trackBar.Location = new System.Drawing.Point(436, 393);
+            this.v_trackBar.Location = new System.Drawing.Point(581, 484);
+            this.v_trackBar.Margin = new System.Windows.Forms.Padding(4);
             this.v_trackBar.Maximum = 100;
             this.v_trackBar.Minimum = -100;
             this.v_trackBar.Name = "v_trackBar";
-            this.v_trackBar.Size = new System.Drawing.Size(206, 45);
+            this.v_trackBar.Size = new System.Drawing.Size(275, 56);
             this.v_trackBar.TabIndex = 5;
             this.v_trackBar.ValueChanged += new System.EventHandler(this.trackBar_ValueChanged);
             // 
             // save_button
             // 
-            this.save_button.Location = new System.Drawing.Point(674, 393);
+            this.save_button.Location = new System.Drawing.Point(887, 464);
+            this.save_button.Margin = new System.Windows.Forms.Padding(4);
             this.save_button.Name = "save_button";
-            this.save_button.Size = new System.Drawing.Size(114, 45);
+            this.save_button.Size = new System.Drawing.Size(152, 55);
             this.save_button.TabIndex = 6;
             this.save_button.Text = "Save changes";
             this.save_button.UseVisualStyleBackColor = true;
+            this.save_button.Click += new System.EventHandler(this.save_button_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(83, 377);
+            this.label1.Location = new System.Drawing.Point(111, 464);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.Size = new System.Drawing.Size(74, 16);
             this.label1.TabIndex = 7;
             this.label1.Text = "Hue [0;360]";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(300, 365);
+            this.label2.Location = new System.Drawing.Point(393, 464);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.Size = new System.Drawing.Size(95, 16);
             this.label2.TabIndex = 8;
             this.label2.Text = "Saturation [0;1]";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(516, 365);
+            this.label3.Location = new System.Drawing.Point(682, 464);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.Size = new System.Drawing.Size(70, 16);
             this.label3.TabIndex = 9;
             this.label3.Text = "Value [0;1]";
             // 
             // HSV_form
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -130,6 +139,7 @@
             this.Controls.Add(this.s_trackBar);
             this.Controls.Add(this.h_trackBar);
             this.Controls.Add(this.pictureBox);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "HSV_form";
             this.Text = "HSV_form";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
--- RGB_HSV_Lab/HSV_form.cs
+++ RGB_HSV_Lab/HSV_form.cs
@@ -1,159 +1,210 @@
-﻿using FastBitmap;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+﻿using System;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
-using static System.Windows.Forms.AxHost;
 
 namespace RGB_HSV_Lab
 {
     public partial class HSV_form : Form
     {
-        Bitmap _bitmap;
+        Bitmap _originalBitmap;
+        Bitmap _currentBitmap;
         HSV_Bitmap _hsv_map;
+        string _originalFileName;
 
         public HSV_form()
         {
             InitializeComponent();
+            _originalFileName = "modified_image";
 
-            _bitmap = new Bitmap(pictureBox.Image);
-            _hsv_map = new HSV_Bitmap(_bitmap);
-            
-            last_h = h_trackBar.Value;
-            last_s = s_trackBar.Value;
-            last_v = v_trackBar.Value;
+            // Сохраняем оригинальное изображение
+            _originalBitmap = new Bitmap(pictureBox.Image);
+            _currentBitmap = new Bitmap(_originalBitmap);
+            _hsv_map = new HSV_Bitmap(_originalBitmap);
+
+            // Инициализируем начальные значения ползунков
+            h_trackBar.Value = 0;
+            s_trackBar.Value = 0;
+            v_trackBar.Value = 0;
         }
 
-
-        int last_h;
-        int last_s;
-        int last_v;
         private void trackBar_ValueChanged(object sender, EventArgs e)
         {
-            _hsv_map.ChangeHSL(ref _bitmap, (h_trackBar.Value - last_h, (s_trackBar.Value - last_s) / 100f, (s_trackBar.Value - last_s) / 100f));
-            pictureBox.Image = _bitmap;
-            last_h = h_trackBar.Value;
-            last_s = s_trackBar.Value;
-            last_v = v_trackBar.Value;
-        }
+            // Всегда работаем от оригинального изображения
+            _currentBitmap = new Bitmap(_originalBitmap);
 
+            _hsv_map.ChangeHSL(ref _currentBitmap,
+                (h_trackBar.Value, s_trackBar.Value, v_trackBar.Value));
+
+            pictureBox.Image = _currentBitmap;
+        }
 
         public class HSV_Bitmap
         {
             private int[,] h_arr;
             private float[,] s_arr, v_arr;
 
-
             public HSV_Bitmap(Bitmap rgb_bitmap)
             {
                 h_arr = new int[rgb_bitmap.Width, rgb_bitmap.Height];
                 s_arr = new float[rgb_bitmap.Width, rgb_bitmap.Height];
                 v_arr = new float[rgb_bitmap.Width, rgb_bitmap.Height];
+
                 for (int i = 0; i < rgb_bitmap.Width; i++)
                 {
                     for (int j = 0; j < rgb_bitmap.Height; j++)
                     {
-                        (h_arr[i,j], s_arr[i, j], v_arr[i, j]) = rgb2hsv(rgb_bitmap.GetPixel(i, j));
+                        (h_arr[i, j], s_arr[i, j], v_arr[i, j]) = rgb2hsv(rgb_bitmap.GetPixel(i, j));
                     }
                 }
             }
 
-            public void ChangeHSL(ref Bitmap rgb_bitmap, (int, float, float) increment)
+            public void ChangeHSL(ref Bitmap rgb_bitmap, (int, int, int) adjustments)
             {
                 for (int i = 0; i < rgb_bitmap.Width; i++)
+                {
                     for (int j = 0; j < rgb_bitmap.Height; j++)
                     {
-                        h_arr[i, j] = (h_arr[i, j] + increment.Item1) % 360;
-                        s_arr[i, j] = s_arr[i, j] + increment.Item2;
-                        v_arr[i, j] = v_arr[i, j] + increment.Item3;
+                        // Применяем изменения к оригинальным HSV значениям
+                        int new_h = (h_arr[i, j] + adjustments.Item1 + 360) % 360;
 
-                        int h = h_arr[i, j];
-                        float s = clamp(s_arr[i, j], 0f, 1f);
-                        float v = clamp(v_arr[i, j], 0f, 1f);
+                        // Для насыщенности: -100..100 → -1.0..1.0
+                        float new_s = clamp(s_arr[i, j] + (adjustments.Item2 / 100f), 0f, 1f);
 
-                        int hi = (int)(h / 60f) % 6;
-                        float f = h / 60f - (int)(h / 60f);
-                        float p = v * (1 - s);
-                        float q = v * (1 - f * s);
-                        float t = v * (1 - (1 - f) * s);
+                        // Для яркости: -100..100 → -1.0..1.0
+                        float new_v = clamp(v_arr[i, j] + (adjustments.Item3 / 100f), 0f, 1f);
 
-                        float r = 0, g = 0, b = 0;
-                        switch (hi)
-                        {
-                            case 0:
-                                r = v; g = t; b = p; break;
-                            case 1:
-                                r = q; g = v; b = p; break;
-                            case 2:
-                                r = p; g = v; b = t; break;
-                            case 3:
-                                r = p; g = q; b = v; break;
-                            case 4:
-                                r = t; g = p; b = v; break;
-                            case 5:
-                                r = v; g = p; b = q; break;
-                        }
-
-                        rgb_bitmap.SetPixel(i, j, Color.FromArgb((byte)(r * 255), (byte)(g * 255), (byte)(b * 255)));
+                        // Конвертируем обратно в RGB
+                        Color newColor = HsvToRgb(new_h, new_s, new_v);
+                        rgb_bitmap.SetPixel(i, j, newColor);
                     }
-                
+                }
             }
 
-            private float clamp (float value, float min, float max)
+            private Color HsvToRgb(int h, float s, float v)
             {
-                if (value < min)
+                h = (h % 360 + 360) % 360;
+                int hi = (h / 60) % 6;
+                float f = h / 60f - hi;
+                float p = v * (1 - s);
+                float q = v * (1 - f * s);
+                float t = v * (1 - (1 - f) * s);
+
+                float r, g, b;
+                switch (hi)
                 {
-                    return min;
-                }
-                else if (value > max)
-                {
-                    return max;
+                    case 0: r = v; g = t; b = p; break;
+                    case 1: r = q; g = v; b = p; break;
+                    case 2: r = p; g = v; b = t; break;
+                    case 3: r = p; g = q; b = v; break;
+                    case 4: r = t; g = p; b = v; break;
+                    case 5: r = v; g = p; b = q; break;
+                    default: r = g = b = 0; break;
                 }
 
+                return Color.FromArgb(
+                    (byte)(r * 255),
+                    (byte)(g * 255),
+                    (byte)(b * 255));
+            }
+
+            private float clamp(float value, float min, float max)
+            {
+                if (value < min) return min;
+                if (value > max) return max;
                 return value;
             }
 
-            private (int, float, float) rgb2hsv (Color rgb)
+            private (int, float, float) rgb2hsv(Color rgb)
             {
-                int h;
-                float s, v;
-                (float r, float g, float b) = (rgb.R / 256f, rgb.G / 256f, rgb.B / 256f);
+                float r = rgb.R / 255f;
+                float g = rgb.G / 255f;
+                float b = rgb.B / 255f;
+
                 float max = Math.Max(r, Math.Max(g, b));
                 float min = Math.Min(r, Math.Min(g, b));
+                float delta = max - min;
 
-                //hue
-                if (max == min)
-                    h = 0;
-                else if (max == r)
+                int h = 0;
+                float s = 0f;
+                float v = max;
+
+                if (delta > 0)
                 {
-                    if (g >= b)
-                        h = (int)(60 * (g - b) / (max - min));
-                    else
-                        h = (int)(60 * (g - b) / (max - min) + 360);
+                    if (max == r)
+                        h = (int)(60 * ((g - b) / delta) % 360);
+                    else if (max == g)
+                        h = (int)(60 * ((b - r) / delta + 2));
+                    else if (max == b)
+                        h = (int)(60 * ((r - g) / delta + 4));
+
+                    if (h < 0) h += 360;
                 }
-                else if (max == g)
-                    h = (int)(60 * (b - r) / (max - min) + 120);
-                else
-                    h = (int)(60 * (r - g) / (max - min) + 240);
 
-                //value
-                v = max;
-
-                //saturation
-                if (max == 0)
-                    s = 0;
-                else
-                    s = 1 - min / max;
+                if (max > 0)
+                    s = delta / max;
 
                 return (h, s, v);
             }
-            
         }
 
+        private void save_button_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog saveDialog = new SaveFileDialog())
+            {
+                saveDialog.Title = "Сохранить изображение с HSV коррекцией";
+                saveDialog.Filter = "PNG Image|*.png|JPEG Image|*.jpg|BMP Image|*.bmp|All Files|*.*";
+                saveDialog.DefaultExt = "png";
+                saveDialog.AddExtension = true;
+
+                string defaultFileName = $"{_originalFileName}_{DateTime.Now:yyyyMMdd_HHmmss}";
+                saveDialog.FileName = defaultFileName;
+
+                if (saveDialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        string filePath = saveDialog.FileName;
+                        System.Drawing.Imaging.ImageFormat format = System.Drawing.Imaging.ImageFormat.Png;
+
+                        if (filePath.EndsWith(".jpg", StringComparison.OrdinalIgnoreCase) ||
+                            filePath.EndsWith(".jpeg", StringComparison.OrdinalIgnoreCase))
+                        {
+                            format = System.Drawing.Imaging.ImageFormat.Jpeg;
+                        }
+                        else if (filePath.EndsWith(".bmp", StringComparison.OrdinalIgnoreCase))
+                        {
+                            format = System.Drawing.Imaging.ImageFormat.Bmp;
+                        }
+
+                        _currentBitmap.Save(filePath, format);
+
+                        MessageBox.Show($"Изображение успешно сохранено!\n\nПуть: {filePath}",
+                                      "Сохранение завершено",
+                                      MessageBoxButtons.OK,
+                                      MessageBoxIcon.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Ошибка при сохранении файла:\n{ex.Message}",
+                                      "Ошибка",
+                                      MessageBoxButtons.OK,
+                                      MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void reset_button_Click(object sender, EventArgs e)
+        {
+            // Сброс ползунков к нулевым значениям
+            h_trackBar.Value = 0;
+            s_trackBar.Value = 0;
+            v_trackBar.Value = 0;
+
+            // Восстановление оригинального изображения
+            _currentBitmap = new Bitmap(_originalBitmap);
+            pictureBox.Image = _currentBitmap;
+        }
     }
 }
